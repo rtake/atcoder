0c05c0218412b67d1b1df28cf6ad756c8b6a52b2
rtake <rtake1110@gmail.com>
2020-05-09 11:52:21 +0900

    [AC] abc017 abc017_2

---
+++ atcoder.jp/abc017/abc017_2/Main.cpp
@@ -0,0 +1,29 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+string x;
+
+int dfs(int index) {
+  int ret = -1;
+
+  if(index == -1) { ret = 1; }
+  else if(index == 1) {
+    if(x[index-1] == 'c' && x[index] == 'h') { ret = 1; }
+  } else if(index == 0) {
+    if(x[index] == 'o' || x[index] == 'k' || x[index] == 'u') { ret = 1; }
+  } else {
+    if(x[index-1] == 'c' && x[index] == 'h') { ret = dfs(index-2); }
+    else if(x[index] == 'o' || x[index] == 'k' || x[index] == 'u') { ret = dfs(index-1); }
+  }
+
+  return ret;
+}
+
+int main() {
+  cin >> x;
+  int ans = dfs( (int)x.size() - 1 );
+
+  if(ans == 1) { cout << "YES\n"; }
+  else { cout << "NO\n"; }
+  return 0;
+}
