965f98349b50c3bda77281d009a3326162745ec4
rtake <rtake1110@gmail.com>
2020-05-11 17:15:17 +0900

    20200511

--- abc107/b.cpp
+++ abc107/b.cpp
@@ -6,7 +6,7 @@ int main() {
   vector<string> a(h); for(int i = 0;i < h;i++) { cin >> a[i]; }
 
   int check, nrow = h, ncol = w;
-  /*
+  
   for(int i = 0;i < nrow;i++) {
     check = 1;
     for(int j = 0;j < w;j++) {
@@ -17,17 +17,15 @@ int main() {
     }
 
     if(check == 1) {
-      if(i < h-1) {
+      if(i < nrow-1) {
         for(int j = i;j < nrow-1;j++) { a[j] = a[j+1]; }
         i--;
       }
       nrow--;
     }
   } // row
-  */
 
   for(int i = 0;i < ncol;i++) {
-
     check = 1;
     for(int j = 0;j < nrow;j++) {
       if(a[j][i] != '.') {
@@ -37,21 +35,17 @@ int main() {
     }
 
     if(check == 1) {
-      if(i < w-1) {
+      if(i < ncol-1) {
         for(int j = i;j < ncol;j++) {
-          for(int k = 0;k < nrow;k++) { a[j][k] = a[j][i+1]; }
+          for(int k = 0;k < nrow;k++) { a[k][j] = a[k][j+1]; }
         }
         i--;
       }
       ncol--;
     }
-
   } // column
 
-  for(int i = 0;i < nrow;i++) {
-    for(int j = 0;j < ncol;j++) { cout << a[i][j]; }
-    cout << endl;
-  }
+  for(int i = 0;i < nrow;i++) { for(int j = 0;j < ncol;j++) { cout << a[i][j]; } cout << endl; }
 
   return 0;
 }
