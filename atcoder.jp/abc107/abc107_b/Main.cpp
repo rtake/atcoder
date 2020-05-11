74af8217d59dd7719c1aa378396442bcd1a0ff3b
rtake <rtake1110@gmail.com>
2020-05-11 17:15:57 +0900

    [AC] abc107 abc107_b

---
+++ atcoder.jp/abc107/abc107_b/Main.cpp
@@ -0,0 +1,51 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int h,w; cin >> h >> w;
+  vector<string> a(h); for(int i = 0;i < h;i++) { cin >> a[i]; }
+
+  int check, nrow = h, ncol = w;
+  
+  for(int i = 0;i < nrow;i++) {
+    check = 1;
+    for(int j = 0;j < w;j++) {
+      if(a[i][j] != '.') {
+        check = -1;
+        break;
+      }
+    }
+
+    if(check == 1) {
+      if(i < nrow-1) {
+        for(int j = i;j < nrow-1;j++) { a[j] = a[j+1]; }
+        i--;
+      }
+      nrow--;
+    }
+  } // row
+
+  for(int i = 0;i < ncol;i++) {
+    check = 1;
+    for(int j = 0;j < nrow;j++) {
+      if(a[j][i] != '.') {
+        check = -1;
+        break;
+      }
+    }
+
+    if(check == 1) {
+      if(i < ncol-1) {
+        for(int j = i;j < ncol;j++) {
+          for(int k = 0;k < nrow;k++) { a[k][j] = a[k][j+1]; }
+        }
+        i--;
+      }
+      ncol--;
+    }
+  } // column
+
+  for(int i = 0;i < nrow;i++) { for(int j = 0;j < ncol;j++) { cout << a[i][j]; } cout << endl; }
+
+  return 0;
+}
