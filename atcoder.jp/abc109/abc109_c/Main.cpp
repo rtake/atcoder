49bfa0ae04a6ca447cadb51749840672970b0e44
rtake <rtake1110@gmail.com>
2020-05-15 21:09:13 +0900

    [AC] abc109 abc109_c

---
+++ atcoder.jp/abc109/abc109_c/Main.cpp
@@ -0,0 +1,20 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+int gcd(int x, int y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+int main() {
+  lint N,X,y; cin >> N >> X;
+  vector<lint> x(N,0);
+  for(lint i=0;i < N;i++) { cin >> y; x[i] = abs(y-X); }
+
+  int d=x[0];
+  for(lint i=1;i<N && d!=1;i++) { d = gcd(x[i],d); }
+
+  cout << d << endl;
+  return 0;
+}
