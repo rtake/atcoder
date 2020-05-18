f133983293afab33e54aec15c14d0559f4ac4403
rtake <rtake1110@gmail.com>
2020-05-18 09:36:31 +0900

    [AC] abc130 abc130_c

---
+++ atcoder.jp/abc130/abc130_c/Main.cpp
@@ -0,0 +1,21 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return abs(x-y)/gcd(x,y); }
+
+
+int main() {
+  int w,h,x,y; cin >> w >> h >> x >> y;
+
+  printf("%.10lf ",(double)w*(double)h/2);
+  if(w%2==0 && h%2==0 && x==w/2 && y==h/2) { printf("1\n"); }
+  else { printf("0\n"); }
+
+  return 0;
+}
