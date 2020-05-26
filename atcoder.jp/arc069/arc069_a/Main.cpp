0e826397dc1115aa37b94295ad72f9604a5e8e24
rtake <rtake1110@gmail.com>
2020-05-26 10:28:16 +0900

    [AC] arc069 arc069_a

---
+++ atcoder.jp/arc069/arc069_a/Main.cpp
@@ -0,0 +1,26 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+
+int main() {
+  lint n,m; cin >> n >> m;
+  if(n >= m/2) { cout << m/2 << endl; }
+  else { cout << (2*n+m)/4 << endl; }
+  return 0;
+}
