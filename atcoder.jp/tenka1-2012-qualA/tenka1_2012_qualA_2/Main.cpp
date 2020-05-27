c98f3fd0aa6481d19adc06c131d05914e348032d
rtake <rtake1110@gmail.com>
2020-05-27 21:53:22 +0900

    [AC] tenka1-2012-qualA tenka1_2012_qualA_2

---
+++ atcoder.jp/tenka1-2012-qualA/tenka1_2012_qualA_2/Main.cpp
@@ -0,0 +1,40 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return abs(x-y)/gcd(x,y); }
+
+
+int main() {
+  string c; //fgets(c,2020,stdin);
+  getline(cin,c); // cout << c << endl;
+  c += '\n'; // cout << c << endl;
+
+  vector<char> vc;
+
+  for(int i=0;i<2020;i++) {
+    if(c[i] == '\n') { break; }
+
+    if(c[i] == ' ') {
+      for(;i<2020;i++) {
+        if(c[i] != ' ') {
+          i--;
+          break;
+        }
+      }
+      vc.push_back(',');
+    } else {
+      vc.push_back(c[i]);
+    }
+  }
+
+  for(int i=0;i<(int)vc.size();i++) { cout << vc[i]; }
+  cout << endl;
+
+  return 0;
+}
