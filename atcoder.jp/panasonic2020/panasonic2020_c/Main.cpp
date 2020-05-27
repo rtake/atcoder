5936d6f6a1c4911fa82ef2e26d78decb377f5ac7
rtake <rtake1110@gmail.com>
2020-05-27 22:49:46 +0900

    [AC] panasonic2020 panasonic2020_c

---
+++ atcoder.jp/panasonic2020/panasonic2020_c/Main.cpp
@@ -0,0 +1,27 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+
+int main() {
+  lint a,b,c; cin >> a >> b >> c;
+
+  if(c-a-b > 0 && (c-a-b)*(c-a-b) > 4*a*b) { cout << "Yes\n"; }
+  else { cout << "No\n"; }
+  return 0;
+}
