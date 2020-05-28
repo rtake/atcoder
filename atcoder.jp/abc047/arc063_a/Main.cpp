7d55bc8c5a6d8673b12b6da81bde54bdded4f311
rtake <rtake1110@gmail.com>
2020-05-28 14:09:26 +0900

    [AC] abc047 arc063_a

---
+++ atcoder.jp/abc047/arc063_a/Main.cpp
@@ -0,0 +1,31 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+
+int main() {
+  string s; cin >> s;
+
+  lint ans=0;
+  for(lint i=1;i<(int)s.size();i++) {
+    if(s[i] != s[i-1]) { ans++; }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
