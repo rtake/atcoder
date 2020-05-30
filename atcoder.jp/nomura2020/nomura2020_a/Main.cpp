0b362452f0f5c20d643f0a8e313e67ce0b8a4f7f
rtake <rtake1110@gmail.com>
2020-05-30 21:02:32 +0900

    [AC] nomura2020 nomura2020_a

---
+++ atcoder.jp/nomura2020/nomura2020_a/Main.cpp
@@ -0,0 +1,33 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+lint P(lint n, lint k) {
+  if(k == 1) { return n; }
+  return (n*(P(n-1,k-1)%1000000007)%1000000007);
+}
+
+
+int main() {
+  int h1,m1,h2,m2,k; cin >> h1 >> m1 >> h2 >> m2 >> k;
+
+  int t1 = h1*60+m1, t2 = h2*60+m2;
+  int ans = (t2-t1)-k;
+  cout << ans << endl;
+  return 0;
+}
