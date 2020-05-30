f08d748c11612730860aaf1fe61d0a674da09966
rtake <rtake1110@gmail.com>
2020-05-30 20:03:09 +0900

    [AC] arc068 arc068_a

---
+++ atcoder.jp/arc068/arc068_a/Main.cpp
@@ -0,0 +1,37 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+lint P(lint n, lint k) {
+  if(k == 1) { return n; }
+  return (n*(P(n-1,k-1)%1000000007)%1000000007);
+}
+
+
+int main() {
+  lint x; cin >> x;
+
+  lint ans;
+  ans = x/11*2;
+
+  if(0 < x%11 && x%11 <= 6) { ans++; }
+  else if(x%11 >= 7) { ans += 2; }
+
+  cout << ans << endl;
+  return 0;
+}
