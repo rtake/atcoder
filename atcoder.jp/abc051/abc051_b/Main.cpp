53d4113a34593bea0dfdc75c7006eebc9faddfb0
rtake <rtake1110@gmail.com>
2020-06-01 02:01:50 +0900

    [AC] abc051 abc051_b

---
+++ atcoder.jp/abc051/abc051_b/Main.cpp
@@ -0,0 +1,42 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+lint P(lint n, lint k) {
+  if(k == 1) { return n; }
+  return (n*(P(n-1,k-1)%1000000007)%1000000007);
+}
+
+
+int main() {
+  lint k,s; cin >> k >> s;
+
+  lint ans=0;
+
+  for(lint x=0;x<=k;x++) {
+    for(lint y=0;y<=k;y++) {
+      lint z=s-x-y;
+      if(0 <= z && z <= k) {
+        ans++;
+      }
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
