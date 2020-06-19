ef5d5d13bd8840bdadd0fb110e6faef88177ea72
rtake <rtake1110@gmail.com>
2020-06-20 00:00:52 +0900

    from vscode 20200619

---
+++ abc125/d.cpp
@@ -0,0 +1,42 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+lint P(lint n, lint k) {
+  if(k == 1) { return n; }
+  return (n*(P(n-1,k-1)%1000000007)%1000000007);
+}
+
+
+int main() {
+  lint n; cin >> n;
+  vector<lint> a(n); for(lint i=0;i<n;i++) { cin >> a[i]; }
+
+  lint cnt=0, abs_min=1000000000, ans=0;
+  for(lint i=0;i<n;i++) {
+    if(abs(a[i]) < abs_min) { abs_min = abs(a[i]); }
+    if(a[i] < 0) { cnt++; }
+    ans += abs(a[i]);
+  }
+
+  if(cnt%2 != 0) { ans -= 2*abs_min; } 
+
+  cout << ans << endl;
+  return 0;
+}
+ 
---
+++ abc148/e.cpp
@@ -0,0 +1,42 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+lint P(lint n, lint k) {
+  if(k == 1) { return n; }
+  return (n*(P(n-1,k-1)%1000000007)%1000000007);
+}
+
+
+int main() {
+  lint n; cin >> n;
+
+  lint value=1, ans=0;
+  for(lint i=n;i>=0;i-=2) {
+    while(true) {
+      if(value%10==0) {
+        value/=10;
+        ans++;
+      } else { break; }
+    }
+    value*=i;
+  }
+  
+  cout << ans << endl;
+  return 0;
+}
