feb8f4204c2a98942f6e15bd77b66ff04d8ae2eb
rtake <rtake1110@gmail.com>
2020-06-29 19:54:26 +0900

    [AC] abc038 abc038_c

---
+++ atcoder.jp/abc038/abc038_c/Main.cpp
@@ -0,0 +1,42 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+lint P(lint n, lint k) {
+  if(k == 1) { return n; }
+  return (n*(P(n-1,k-1)%1000000007)%1000000007);
+}
+
+
+int main() {
+  lint n; cin >> n;
+  vector<lint> a(n); for(lint i=0;i<n;i++) { cin >> a[i]; }
+
+  vector<lint> c(n,1);
+  lint cnt=1;
+  for(lint i=n-2;i>=0;i--) {
+    if(a[i]<a[i+1]) { c[i] = ++cnt; }
+    else { cnt = 1; }
+  }
+
+  lint ans=0;
+  for(lint i=0;i<n;i++) { ans += c[i]; }
+
+  cout << ans << endl;
+  return 0;
+}
