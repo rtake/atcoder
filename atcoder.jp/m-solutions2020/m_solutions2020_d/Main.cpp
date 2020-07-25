262c0a970c79f6abfcab197087c6306c2c2812c5
rtake <rtake1110@gmail.com>
2020-07-25 21:42:51 +0900

    [AC] m-solutions2020 m_solutions2020_d

---
+++ atcoder.jp/m-solutions2020/m_solutions2020_d/Main.cpp
@@ -0,0 +1,38 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+lint P(lint n, lint k) {
+  if(k == 1) { return n; }
+  return (n*(P(n-1,k-1)%1000000007)%1000000007);
+}
+
+
+int main() {
+  lint n; cin >> n;
+  vector<lint> a(n); for(lint i=0;i<n;i++) { cin >> a[i]; }
+
+  lint ans=1000;
+  for(lint i=1;i<n;i++) {
+    if(a[i] <= a[i-1]) { continue; }
+    ans += (ans/a[i-1])*(a[i]-a[i-1]);
+  }
+
+  cout << ans << endl;
+  return 0;
+}
