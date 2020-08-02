b185efb441db47f08bfd0ca0e3cc9da39f9f7000
rtake <rtake1110@gmail.com>
2020-08-02 21:05:47 +0900

    [AC] abc174 abc174_b

---
+++ atcoder.jp/abc174/abc174_b/Main.cpp
@@ -0,0 +1,37 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) {
+  if(x == 0) { return y; }
+  else { return gcd(y%x,x); }
+}
+
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+
+lint C(lint n, lint k) {
+  if(n == k) { return 1; }
+  else if(n < k) { return 0; }
+  else if(k == 0) { return 1; }
+  else if(k == 1) { return n; }
+  else return C(n-1,k-1) + C(n-1,k);
+}
+
+lint P(lint n, lint k) {
+  if(k == 1) { return n; }
+  return (n*(P(n-1,k-1)%1000000007)%1000000007);
+}
+
+
+int main() {
+  lint n,d; cin >> n >> d;
+  vector<lint> x(n),y(n); for(lint i=0;i<n;i++) { cin >> x[i] >> y[i]; }
+
+  lint ans=0;
+  for(int i=0;i<n;i++) {
+    if(x[i]*x[i]+y[i]*y[i] <= d*d) { ans++; }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
