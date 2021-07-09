fb99ca8d66e374c2454f17359b6882afd9380a20
rtake <rtake1110@gmail.com>
2021-07-09 23:04:12 +0900

    [AC] abc002 abc002_3

---
+++ atcoder.jp/abc002/abc002_3/Main.cpp
@@ -0,0 +1,25 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%1000000007;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%1000000007;
+}
+
+
+int main() {
+  int a[2], b[2], c[2];
+  cin >> a[0] >> a[1] >> b[0] >> b[1] >> c[0] >> c[1];
+
+  printf("%.12lf\n", (double)abs((a[0]-c[0])*(b[1]-c[1])-(b[0]-c[0])*(a[1]-c[1]))/2);
+  return 0;
+}
