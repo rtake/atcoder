0f67a7c2657a4a70aa2f01f4223a534c9c27b865
rtake <rtake1110@gmail.com>
2021-07-10 15:22:36 +0900

    [AC] abc060 abc060_b

---
+++ atcoder.jp/abc060/abc060_b/Main.cpp
@@ -0,0 +1,32 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%1000000007;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%1000000007;
+}
+
+
+int main() {
+  int a,b,c; cin >> a >> b >> c;
+
+  int modsum=0;
+  for(int i=1;i<=b-1;i++) {
+    if((a*i)%b == c) {
+      cout << "YES\n";
+      return 0;
+    }
+  }
+
+  cout << "NO\n";
+  return 0;
+}
