4413c0dc964a553ca1441680a3782e4be7330052
rtake <rtake1110@gmail.com>
2021-07-10 15:04:56 +0900

    [AC] abc174 abc174_d

---
+++ atcoder.jp/abc174/abc174_d/Main.cpp
@@ -0,0 +1,36 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%1000000007;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%1000000007;
+}
+
+
+int main() {
+  int n; cin >> n;
+  vector<char> c(n); for(int i=0;i<n;i++) cin >> c[i];
+
+  int rcnt=0, wcnt=0;
+  for(auto x:c) {
+    if(x == 'R') rcnt++;
+    else if(x == 'W') wcnt++;
+  }
+
+  int ans=rcnt;
+  for(int i=0;i<rcnt;i++) {
+    if(c[i] == 'R') ans--;
+  }
+
+  printf("%d\n", min(ans, min(rcnt,wcnt) ));
+  return 0;
+}
