f2f144d2c4208c367b63e02893fca7d6214318c8
rtake <rtake1110@gmail.com>
2021-07-10 20:46:12 +0900

    [AC] abc098 abc098_b

---
+++ atcoder.jp/abc098/abc098_b/Main.cpp
@@ -0,0 +1,40 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%1000000007;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%1000000007;
+}
+
+
+int main() {
+  int n; cin>>n;
+  string s; cin>>s;
+
+  int ans=0;
+  for(int i=1;i<n-1;i++) {
+    map<char,int> mp0, mp1;
+
+    for(int j=0;j<n;j++) {
+      if(j<i) mp0[s[j]] = 1;
+      else mp1[s[j]] = 1;
+    }
+
+    int cnt=0;
+    for(auto x:mp0) cnt+=mp1[x.first];
+
+    ans = max(ans,cnt);
+  }
+
+  cout << ans << endl;
+  return 0;
+}
