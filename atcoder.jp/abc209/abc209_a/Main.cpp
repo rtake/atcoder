6ce9e8775967bb0c178a2333d2248176c425d106
rtake <rtake1110@gmail.com>
2021-07-10 21:05:33 +0900

    [AC] abc209 abc209_a

---
+++ atcoder.jp/abc209/abc209_a/Main.cpp
@@ -0,0 +1,25 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%1000000007;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%1000000007;
+}
+
+
+int main() {
+  int a,b; cin>>a>>b;
+  if(a>=b) cout << 0 << endl;
+  else cout << b-a+1 << endl;
+
+  return 0;
+}
