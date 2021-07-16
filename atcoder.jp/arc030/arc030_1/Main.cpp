19f6f10a593542ac18094a211513091cd430890b
rtake <rtake1110@gmail.com>
2021-07-16 22:37:53 +0900

    [AC] arc030 arc030_1

---
+++ atcoder.jp/arc030/arc030_1/Main.cpp
@@ -0,0 +1,26 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%1000000007;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%1000000007;
+}
+
+
+int main() {
+  int n,k; cin>>n>>k;
+
+  if(k <= n/2) cout << "YES\n";
+  else cout << "NO\n";
+
+  return 0;
+}
