58fa7d2a05269369ce6fefa9849f3e5d24de1ab3
rtake <rtake1110@gmail.com>
2021-07-16 19:43:25 +0900

    [AC] arc109 arc109_b

---
+++ atcoder.jp/arc109/arc109_b/Main.cpp
@@ -0,0 +1,28 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%1000000007;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%1000000007;
+}
+
+
+int main() {
+  lint n; cin>>n;
+
+  lint k=0;
+  while(n+1 >= k*(k+1)/2) k++;
+  k--;
+
+  printf("%lld\n", (n+1)-k);
+  return 0;
+}
