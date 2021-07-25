d43b84c1db2185ed540e33f9ce5589ce319a8897
rtake <rtake1110@gmail.com>
2021-07-25 22:05:37 +0900

    [AC] arc124 arc124_b

---
+++ atcoder.jp/arc124/arc124_b/Main.cpp
@@ -0,0 +1,46 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+#define rep(i,n) for (lint i=0; i<(lint)(n);i++)
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%1000000007;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%1000000007;
+}
+
+
+int main() {
+  lint n; cin>>n;
+  vector<lint> a(n),b(n); rep(i,n) cin>>a[i]; rep(i,n) cin>>b[i];
+
+  set<lint> xsub, bst, x;
+  rep(i,n) {
+    bst.insert(b[i]);
+    xsub.insert(a[0]^b[i]);
+  }
+
+  for(auto xitr:xsub) {
+    x.insert(xitr);
+
+    rep(i,n) {
+      if(bst.find(xitr^a[i]) == bst.end()) {
+        x.erase(xitr);
+        break;
+      }
+    }
+  }
+
+  printf("%lld\n", (lint)x.size());
+  for(auto xitr:x) printf("%lld\n", (lint)xitr);
+
+  return 0;
+}
