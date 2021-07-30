647fd447dc93be8ca46dbd0e8d3b64e956cbb503
rtake <rtake1110@gmail.com>
2021-07-31 02:55:22 +0900

    [AC] abc152 abc152_d

---
+++ atcoder.jp/abc152/abc152_d/Main.cpp
@@ -0,0 +1,47 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+#define rep(i,n) for (lint i=0; i<(lint)(n);i++)
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint mod=1000000007;
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%mod;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%mod;
+}
+
+lint inv(lint x) {
+  lint res=1, k=mod-2;
+  while(k>0) {
+    if(k&1 == 1) res=(res*x)%mod;
+    x=(x*x)%mod;
+    k/=2;
+  }
+  return res;
+}
+
+int main() {
+  lint n; cin>>n;
+
+  vector< vector<lint> > c(10, vector<lint>(10,0));
+  for(int i=1;i<=n;i++) {
+    stringstream ss;
+    ss<<i;
+    string s(ss.str());
+    c[s[0]-'0'][s[s.length()-1]-'0']++;
+  }
+
+  lint ans=0;
+  rep(i,10) rep(j,10) ans+=c[i][j]*c[j][i];
+
+  printf("%lld\n", ans);
+  return 0;
+}
