2197ec9114573df60c478a5406b2b93998cba867
rtake <rtake1110@gmail.com>
2021-07-31 21:17:36 +0900

    [AC] abc212 abc212_b

---
+++ atcoder.jp/abc212/abc212_b/Main.cpp
@@ -0,0 +1,66 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+#define rep(i,n) for (lint i=0; i<(lint)(n);i++)
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint mod=1000000007;
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%mod;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%mod;
+}
+
+lint inv(lint x) {
+  lint res=1, k=mod-2;
+  while(k>0) {
+    if(k&1 == 1) res=(res*x)%mod;
+    x=(x*x)%mod;
+    k/=2;
+  }
+  return res;
+}
+
+lint nCr_mod(lint n, lint k) {
+  lint a=1,b=1;
+  for(int i=0;i<k;i++) a=(a*(n-i))%mod;
+  for(int i=0;i<k;i++) b=(b*(k-i))%mod;
+  return (a*inv(b))%mod;
+}
+
+int main() {
+  string x; cin>>x;
+
+  bool is_weak=true;
+  for(int i=0;i<3;i++) {
+    if(x[i] != x[i+1]) {
+      is_weak=false;
+      break;
+    }
+  }
+
+  if(is_weak) {
+    cout<<"Weak\n";
+    return 0;
+  }
+
+  for(int i=0;i<3;i++) {
+    if(x[i+1]%10 == (x[i]+1)%10) {
+      is_weak=true;
+    } else {
+      cout<<"Strong\n";
+      return 0;
+    }
+  }
+
+  if(is_weak) cout<<"Weak\n";
+
+  return 0;
+}
