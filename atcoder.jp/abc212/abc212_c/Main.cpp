d68ca541be1d8b5332e2bfcace266e7e5b1d9c1f
rtake <rtake1110@gmail.com>
2021-07-31 21:32:45 +0900

    [AC] abc212 abc212_c

---
+++ atcoder.jp/abc212/abc212_c/Main.cpp
@@ -0,0 +1,60 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+#define rep(i,n) for (lint i=0; i<(lint)(n);i++)
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint mod=1000000007;
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%mod;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%mod;
+}
+
+lint inv(lint x) {
+  lint res=1, k=mod-2;
+  while(k>0) {
+    if(k&1 == 1) res=(res*x)%mod;
+    x=(x*x)%mod;
+    k/=2;
+  }
+  return res;
+}
+
+lint nCr_mod(lint n, lint k) {
+  lint a=1,b=1;
+  for(int i=0;i<k;i++) a=(a*(n-i))%mod;
+  for(int i=0;i<k;i++) b=(b*(k-i))%mod;
+  return (a*inv(b))%mod;
+}
+
+int main() {
+  lint n,m; cin>>n>>m;
+  vector<lint> a(n),b(m);
+  rep(i,n) cin>>a[i];
+  rep(i,m) cin>>b[i];
+
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+
+  lint cur=0, ans=abs(a[0]-b[0]);
+  for(int i=0;i<n;i++) {
+
+    while(cur<m) {
+      ans=min(ans,abs(a[i]-b[cur]));
+      if(b[cur]>a[i]) break;
+      cur++;
+    }
+
+  }
+
+  printf("%lld\n", ans);
+  return 0;
+}
