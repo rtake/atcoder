f40fa62698654e1c87fa2e9dab52bab5d3450bc3
rtake <rtake1110@gmail.com>
2021-08-01 09:31:54 +0900

    [AC] abc115 abc115_a

---
+++ atcoder.jp/abc115/abc115_a/Main.cpp
@@ -0,0 +1,46 @@
+# include <bits/stdc++.h>
+typedef long long lint;
+using namespace std;
+
+#define rep(i,n) for (lint i=0; i<(lint)(n);i++)
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint mod=1000000007;
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%mod;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%mod;
+}
+
+lint inv(lint x) {
+  lint res=1, k=mod-2;
+  while(k>0) {
+    if(k&1 == 1) res=(res*x)%mod;
+    x=(x*x)%mod;
+    k/=2;
+  }
+  return res;
+}
+
+lint nCr_mod(lint n, lint k) {
+  lint a=1,b=1;
+  for(int i=0;i<k;i++) a=(a*(n-i))%mod;
+  for(int i=0;i<k;i++) b=(b*(k-i))%mod;
+  return (a*inv(b))%mod;
+}
+
+int main() {
+  int d; cin>>d;
+  if(d == 25) cout<<"Christmas\n";
+  else if(d == 24) cout<<"Christmas Eve\n";
+  else if(d == 23) cout<<"Christmas Eve Eve\n";
+  else if(d == 22) cout<<"Christmas Eve Eve Eve\n";
+
+  return 0;
+}
