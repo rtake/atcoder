8ea45731582a01ea6a1403b8d48ba77430963a56
rtake <rtake1110@gmail.com>
2021-08-04 12:23:27 +0900

    [AC] typical90 typical90_b

---
+++ atcoder.jp/typical90/typical90_b/Main.cpp
@@ -0,0 +1,77 @@
+# include <bits/stdc++.h>
+//# include <atcoder/all>
+
+typedef long long lint;
+
+using namespace std;
+//using namespace atcoder;
+
+#define rep(i,n) for (lint i=0; i<(lint)(n);i++)
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint mod=1000000007;
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%mod;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%mod;
+}
+
+lint inv(lint x) {
+  lint res=1, k=mod-2;
+  while(k>0) {
+    if(k&1 == 1) res=(res*x)%mod;
+    x=(x*x)%mod;
+    k/=2;
+  }
+  return res;
+}
+
+lint nCr_mod(lint n, lint k) {
+  lint a=1,b=1;
+  for(int i=0;i<k;i++) a=(a*(n-i))%mod;
+  for(int i=0;i<k;i++) b=(b*(k-i))%mod;
+  return (a*inv(b))%mod;
+}
+
+// Segment tree
+lint op(lint a, lint b) { return a^b; }
+lint e() { return 0LL; }
+
+int main() {
+  int n; cin>>n;
+
+  set<string> st;
+  for(int bit=0;bit<(1<<n);bit++) {
+
+    int sum=0;
+    rep(i,n) {
+
+      if(bit&(1<<i)) sum++;
+      else sum--;
+
+      // printf("%d %d %d %d\n", bit, 1<<i, bit&(1<<i), sum);
+
+      if(sum<0) break;
+    }
+
+    if(sum != 0) continue;
+
+    string s;
+    rep(i,n) {
+      if(bit&(1<<i)) s+="(";
+      else s+=")";
+    }
+
+    st.insert(s);
+  }
+
+  for(auto x:st) cout<<x<<endl;
+
+  return 0;
+}
