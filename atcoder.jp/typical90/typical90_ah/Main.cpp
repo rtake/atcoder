f9d68c0cfefecd3c2108f44a8e73b60e4c002760
rtake <rtake1110@gmail.com>
2021-08-13 14:38:43 +0900

    [AC] typical90 typical90_ah

---
+++ atcoder.jp/typical90/typical90_ah/Main.cpp
@@ -0,0 +1,110 @@
+# include <bits/stdc++.h>
+//# include <atcoder/all>
+
+typedef long long lint;
+
+using namespace std;
+//using namespace atcoder;
+
+#define rep(i,n) for (lint i=0; i<(lint)(n);i++)
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint mod=1000000007;
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%mod;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%mod;
+}
+
+lint inv(lint x) {
+  lint res=1, k=mod-2;
+  while(k>0) {
+    if(k&1 == 1) res=(res*x)%mod;
+    x=(x*x)%mod;
+    k/=2;
+  }
+  return res;
+}
+
+lint nCr_mod(lint n, lint k) {
+  lint a=1,b=1;
+  for(int i=0;i<k;i++) a=(a*(n-i))%mod;
+  for(int i=0;i<k;i++) b=(b*(k-i))%mod;
+  return (a*inv(b))%mod;
+}
+
+// Segment tree
+lint op(lint a, lint b) { return a^b; }
+lint e() { return 0LL; }
+
+struct UnionFind {
+    vector<int> par;
+
+    UnionFind(int N) : par(N) {
+        for(int i = 0; i < N; i++) par[i] = i;
+    }
+
+    int root(int x) {
+        if (par[x] == x) return x;
+        return par[x] = root(par[x]);
+    }
+
+    void unite(int x, int y) {
+        int rx = root(x);
+        int ry = root(y);
+        if (rx == ry) return;
+        par[rx] = ry;
+    }
+
+    bool same(int x, int y) {
+        int rx = root(x);
+        int ry = root(y);
+        return rx == ry;
+    }
+};
+
+int main() {
+  lint n,k;
+  cin>>n>>k;
+  vector<lint> a(n);
+  rep(i,n) cin>>a[i];
+
+  lint l=0,r=0,cnt=0,ans=0;
+  map<lint,lint> mp;
+
+  while(r<n) {
+    /*
+    cout<<l<<" "<<r<<endl;
+    cout<<"mp ";
+    for(auto x:mp) cout<<x.first<<" "<<x.second<<" ";
+    cout<<endl;
+    */
+
+    if(mp[a[r]] == 0) {
+      if(cnt<k) {
+        cnt++;
+      } else {
+        while(l<=r) {
+          mp[a[l]]--;
+          l++;
+          if(mp[a[l-1]] == 0) break;
+        }
+      }
+    }
+
+    mp[a[r]]++;
+
+    ans=max(ans,r-l+1);
+    r++;
+  }
+
+  printf("%lld\n", ans);
+
+  return 0;
+}
