13ade0733e597d11f51c3b09cf5341ecf82d7867
rtake <rtake1110@gmail.com>
2021-08-22 16:02:58 +0900

    [AC] abc002 abc002_4

---
+++ atcoder.jp/abc002/abc002_4/Main.cpp
@@ -0,0 +1,153 @@
+# include <bits/stdc++.h>
+//# include <atcoder/all>
+
+typedef long long lint;
+typedef long long ll;
+
+using namespace std;
+//using namespace atcoder;
+
+#define rep(i,n) for (lint i=0; i<(lint)(n);i++)
+
+lint gcd(lint x, lint y) { return (x==0)? y : gcd(y%x,x); }
+lint lcm(lint x, lint y) { return x/gcd(x,y)*y; }
+lint P(lint n, lint k) { return (k==1) ? n : n*(P(n-1,k-1)); }
+
+lint mod=1000000007;
+lint comb[2000][2000];
+lint nCr(lint n, lint r) {
+  if(n==r) return comb[n][r] = 1;
+  if(r==0) return comb[n][r] = 1;
+  if(r==1) return comb[n][r] = n;
+  if(comb[n][r]) return comb[n][r]%mod;
+  return comb[n][r] = (nCr(n-1,r) + nCr(n-1,r-1))%mod;
+}
+
+lint inv(lint x) {
+  lint res=1, k=mod-2;
+  while(k>0) {
+    if(k&1 == 1) res=(res*x)%mod;
+    x=(x*x)%mod;
+    k/=2;
+  }
+  return res;
+}
+
+lint nCr_mod(lint n, lint k) {
+  lint a=1,b=1;
+  for(int i=0;i<k;i++) a=(a*(n-i))%mod;
+  for(int i=0;i<k;i++) b=(b*(k-i))%mod;
+  return (a*inv(b))%mod;
+}
+
+// Segment tree
+lint op(lint a, lint b) { return a^b; }
+lint e() { return 0LL; }
+
+/*
+struct UnionFind {
+    vector<int> par;
+
+    UnionFind(int N) : par(N) {
+        for(int i = 0; i < N; i++) par[i] = i;
+    }
+
+    int root(int x) {
+        if (par[x] == x) return x;
+        return par[x] = root(par[x]);
+    }
+
+    void unite(int x, int y) {
+        int rx = root(x);
+        int ry = root(y);
+        if (rx == ry) return;
+        par[rx] = ry;
+    }
+
+    bool same(int x, int y) {
+        int rx = root(x);
+        int ry = root(y);
+        return rx == ry;
+    }
+};
+*/
+
+
+// https://ei1333.github.io/luzhiled/snippets/structure/union-find.html
+struct UnionFind {
+  vector<ll> data; // store root | (-size)
+
+  UnionFind(ll sz) { data.assign(sz, -1); }
+
+  bool unite(ll x, ll y) {
+    x=find(x);
+    y=find(y);
+    if(x == y) return false;
+    if(data[x] > data[y]) swap(x,y);
+    data[x] += data[y]; // size
+    data[y] = x; // root
+    return true;
+  }
+
+  int find(int k) {
+    if(data[k] < 0) return k;
+    return data[k]=find(data[k]);
+  }
+
+  int size(int k) { return (-data[find(k)]); }
+};
+
+
+ll binpower(ll a, ll b) {
+  ll ans=1;
+  while (b != 0) {
+    if (b%2 == 1) ans = (ans*a)%mod;
+    a=(a*a)%mod;
+    b/=2;
+  }
+  return ans;
+}
+
+
+int main() {
+  int n,m;
+  cin>>n>>m;
+
+  int c[n][n];
+  rep(i,n) rep(j,n) c[i][j]=0;
+  rep(i,n) c[i][i]=1;
+
+  int x,y;
+  rep(i,m) {
+    cin>>x>>y;
+    x--;
+    y--;
+    c[x][y]=c[y][x]=1;
+  }
+
+  int ans=0;
+  for(int bit=0;bit<(1<<n);bit++) {
+    vector<int> v;
+    rep(i,n) if((bit&(1<<i)) > 0) {
+      // cout<<"1<<i "<<(1<<i)<<" bit& "<<(bit&(1<<i))<<endl;
+      v.push_back(i);
+    }
+
+    bool is_acceptable=true;
+    rep(i,v.size()) rep(j,v.size()) {
+      if(c[v[i]][v[j]] != 1) is_acceptable=false;
+    }
+
+    /*
+    for(auto x:v) cout<<x<<" ";
+    cout<<endl;
+    */
+
+    if(is_acceptable) ans=max(ans,(int)v.size());
+    // cout<<v.size()<<endl;
+  }
+
+  cout<<ans<<endl;
+
+  return 0;
+}
